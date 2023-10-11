2590ada79c662af412ec00d6a27b0036e7b40b85
Joel Savitz <joel@kdlp.underground.software>
2023-10-11 04:36:21 -0400

    include links to chat and all columns of data
    Signed-off-by: Joel Savitz <joel@kdlp.underground.software>

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,35 @@
 function getContacts() {
-		all = document.getElementsByTagName("span");
-		for (i = 0; i < all.length; i++) {
-				if (all[i].hasAttribute("title")) {
-						ttl = all[i].getAttribute("title")
-						csv = ttl.split(',')
-						if (csv.length > 20) {
-								bl = new Blob([csv], {type: "text/csv"})
-								window.open(window.webkitURL.createObjectURL(bl))
-						}
+all = document.getElementsByTagName("span");
+	for (i = 0; i < all.length; i++) {
+		if (all[i].hasAttribute("title")) {
+			ttl = all[i].getAttribute("title")
+			csv = ttl.split(',')
+			res = ""
+			for (j = 0; j < csv.length; j++) {
+				if (j > 5000) {
+					break
 				}
+				console.log("jter " + j)
+				console.log(csv[j])
+				console.log(csv[j][0])
+				next = csv[j].trim()
+				if (next[0]== "+") {
+					res += next + "," + '=HYPERLINK("https://wa.me/' + next.replace(/\D/g, '') + '")' + '\n'
+				}
+			}
+			if (res.length > 3) {
+				bl = new Blob([res], {type: "text/csv"})
+				window.open(window.webkitURL.createObjectURL(bl))
+			}
 		}
-
-
+	}
 }
 
 chrome.action.onClicked.addListener((tab) => {
-		if (!tab.url.includes('chrome://')) {
-				chrome.scripting.executeScript({
-						target: { tabId: tab.id },
-						function: getContacts
-				});
-		}
+	if (!tab.url.includes('chrome://')) {
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			function: getContacts
+		});
+	}
 });
